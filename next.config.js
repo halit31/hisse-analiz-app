3e46786b1f475e980ce258e4a43fe30f40ca4bc4
halit31 <137624312+halit31@users.noreply.github.com>
2026-04-22 22:15:48 +0300

    Create next.config.js file with Next.js 14 configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+};
+
+module.exports = nextConfig;
